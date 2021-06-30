3d2fccf4b3cb1061dc15a16d359b0a8f14e729e7
rawallon <rawallon@gmail.com>
2021-06-30 15:23:39 -0300

    Removed Home and NewRoom pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AuthContextProvider } from './contexts/AuthContext';
 
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
 import { RoomListing } from './pages/RoomListing';
 
@@ -11,10 +9,8 @@ function App() {
     <BrowserRouter>
       <AuthContextProvider>
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
-          <Route path="/list" component={RoomListing} />
+          <Route path="/" component={RoomListing} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
