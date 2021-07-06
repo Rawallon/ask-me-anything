543f32869b080b26e9c419a80d4744fc1721cc0e
rawallon <rawallon@gmail.com>
2021-07-06 11:28:35 -0300

    Updated Rooms context to use Firebase Utils

--- src/contexts/RoomsContext.tsx
+++ src/contexts/RoomsContext.tsx
@@ -1,21 +1,14 @@
-import { createContext, useState, useEffect, ReactNode } from 'react';
+import {
+  createContext,
+  useState,
+  useEffect,
+  ReactNode,
+  useCallback,
+} from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
+import { mapQuestionsData, mapRoomData, retrieveData } from '../utils/firebase';
 
-type FirebaseReturn = Record<
-  string,
-  {
-    title: string;
-    description?: string;
-    author: {
-      id: string;
-      name: string;
-      avatar: string;
-    };
-    endedAt: string | null;
-    questions: any[];
-  }
->;
 type QuestionType = {
   id: string;
   author: {
@@ -28,7 +21,7 @@ type QuestionType = {
   isUserDeleted: boolean;
   isAdminDeleted: boolean;
   likeCount: number;
-  likeId: string | undefined;
+  likeId: any;
 };
 
 type RoomType = {
@@ -51,7 +44,7 @@ type RoomsContextType = {
     description: string,
   ) => Promise<string | null | undefined>;
   isLoadingRooms: boolean;
-  handleLoadQuestions: (roomId: string) => QuestionType[];
+  handleLoadQuestions: (roomId: string) => Promise<QuestionType[]>;
   isLoadingQuestions: boolean;
   filterRoom: (roomId: string) => RoomType;
 };
@@ -60,65 +53,34 @@ type RoomsContextProviderProps = {
   children: ReactNode;
 };
 
-type roomQuestionsType = Record<
-  string,
-  {
-    author: {
-      name: string;
-      avatar: string;
-    };
-    content: string;
-    isAnswered: boolean;
-    isHighlighted: boolean;
-    isUserDeleted: boolean;
-    isAdminDeleted: boolean;
-    likes: Record<
-      string,
-      {
-        authorId: string;
-      }
-    >;
-  }
->;
-
 export const RoomsContext = createContext({} as RoomsContextType);
 
 export function RoomsContextProvider(props: RoomsContextProviderProps) {
   const [rooms, setRooms] = useState<RoomType[]>([]);
   const [isLoadingRooms, setIsLoadingRooms] = useState(true);
-  const [isLoadingQuestions, setIsLoadingQuestions] = useState(true);
+  const [isLoadingQuestions, setIsLoadingQuestions] = useState(false);
   const { user } = useAuth();
 
+  const fetchRooms = useCallback(async () => {
+    setIsLoadingRooms(true);
+    const rooms = await retrieveData('rooms');
+    const mappedRooms = await mapRoomData(rooms);
+    // Ended rooms go to bottom, then sorts by number of comments
+    setRooms(
+      mappedRooms.sort((a, b) =>
+        Number(!!b.endedAt) > Number(!!a.endedAt)
+          ? -1
+          : a.questionNum < b.questionNum
+          ? 0
+          : -1,
+      ),
+    );
+    setIsLoadingRooms(false);
+  }, []);
+
   useEffect(() => {
-    try {
-      database.ref('rooms').on('value', function (snapshot) {
-        const databaseRooms = snapshot.val();
-        const mappedRooms = Object.entries(
-          (databaseRooms as FirebaseReturn) ?? {},
-        ).map(([key, value]) => ({
-          id: key,
-          title: value.title,
-          description: value.description,
-          author: value.author,
-          endedAt: value.endedAt,
-          questionNum: Object.keys(value.questions || {}).length,
-        }));
-        // Ended rooms go to bottom, then sorts by number of comments
-        setRooms(
-          mappedRooms.sort((a, b) =>
-            Number(!!b.endedAt) > Number(!!a.endedAt)
-              ? -1
-              : a.questionNum < b.questionNum
-              ? 0
-              : -1,
-          ),
-        );
-        setIsLoadingRooms(false);
-      });
-    } catch {
-      console.error('Ocorreu um erro ao carregar as salas');
-    }
-  }, [user]);
+    fetchRooms();
+  }, [fetchRooms]);
 
   async function handleCreateRoom(title: string, description: string) {
     if (title.trim() === '' || !user) {
@@ -137,43 +99,21 @@ export function RoomsContextProvider(props: RoomsContextProviderProps) {
     return firebaseRoom ? firebaseRoom.key : null;
   }
 
-  function handleLoadQuestions(roomId: string): QuestionType[] {
+  const handleLoadQuestions = useCallback(async (roomId: string) => {
     setIsLoadingQuestions(true);
-    const roomRef = database.ref(`rooms/${roomId}/questions`);
-    let retQuestions: QuestionType[] = [];
-    roomRef.on('value', (questions) => {
-      const roomQuestions = questions.val();
-      if (!roomQuestions) {
-        setIsLoadingQuestions(false);
-        return;
-      }
-      const parsedQuestions: QuestionType[] = Object.entries(
-        (roomQuestions as roomQuestionsType) || {},
-      ).map(([key, value]) => {
-        return {
-          id: key,
-          content: value.content,
-          author: value.author,
-          isHighlighted: value.isHighlighted,
-          isAnswered: value.isAnswered,
-          isUserDeleted: value.isUserDeleted,
-          isAdminDeleted: value.isAdminDeleted,
-          likeCount: Object.values(value.likes ?? {}).length,
-          likeId: Object.entries(value.likes ?? {}).find(
-            ([key, like]) => like.authorId === user?.id,
-          )?.[0],
-        };
-      });
-      setIsLoadingQuestions(false);
-      retQuestions = parsedQuestions;
-    });
-    return retQuestions;
-  }
+    const questions = await retrieveData(`rooms/${roomId}/questions`);
+    const mappedQuestions = await mapQuestionsData(questions);
+    setIsLoadingQuestions(false);
+    return mappedQuestions;
+  }, []);
 
-  function filterRoom(roomId: string) {
-    const filteredRoom = rooms.filter((room) => room.id === roomId)[0];
-    return filteredRoom || null;
-  }
+  const filterRoom = useCallback(
+    (roomId: string) => {
+      const filteredRoom = rooms.filter((room) => room.id === roomId)[0];
+      return filteredRoom || null;
+    },
+    [rooms],
+  );
 
   return (
     <RoomsContext.Provider
